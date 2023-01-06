235c305919b7a9b86830205beb88e31c73e8c67d
julienDeveaux <julien.deveaux@hotmail.com>
2023-01-06 17:39:19 +0100

    feat: add socket support, command route & websocket test page

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
 from flask import Flask
+from flask import render_template
+from flask_sock import Sock
 
 app = Flask(__name__)
+sock = Sock(app)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    return render_template('index.html', name="Hi mom")
+
+
+@sock.route('/echo')
+def echo_socket(sock):
+    while True:
+        message = sock.receive()
+        sock.send(message)
+
+
+@app.route('/commande/<id>', methods=['GET'])
+def commande(id):
+    match id:
+        case '0':
+            return 'up'
+        case '1':
+            return 'down'
+        case '2':
+            return 'left'
+        case '3':
+            return 'right'
+        case _:
+            return 'error'
 
 
 if __name__ == '__main__':
